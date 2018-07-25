682c5b6e7b8ec1b23b0c3a8c4f2698118b5330b0
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2018-07-26 00:49:47 +0700

    modify templates & static

--- bdfi/urls.py
+++ bdfi/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from movie import views as movie_views
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', movie_views.home, name='home')
 ]
