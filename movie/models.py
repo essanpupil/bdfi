39aef09dba3cda14bd5f218e0b4775ae74165ccc
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2018-07-27 02:20:57 +0700

    add models Movie & Actor

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=255)
+    birth_of_date = models.DateField()
+    movie = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return self.name
