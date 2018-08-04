3e0144bad98dfefeda13bcec08f2455e544b8325
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2018-08-04 18:13:48 +0700

    add modles category & genre

--- movie/models.py
+++ movie/models.py
@@ -2,9 +2,25 @@ from django.db import models
 from embed_video.fields import EmbedVideoField
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     release_date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, null=True)
     video = EmbedVideoField(null=True)
 
     def __str__(self):
