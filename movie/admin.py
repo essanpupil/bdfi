affd379f9cdecdcae0240cf590308c4b27f0f888
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2018-08-04 22:16:38 +0700

    Display genre

--- movie/admin.py
+++ movie/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from movie.models import Movie, Actor
+from movie.models import Movie, Actor, Genre
 
 admin.site.register(Movie)
 admin.site.register(Actor)
+admin.site.register(Genre)
--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-from movie.models import Movie
+from movie.models import Movie, Genre
 
 
 def home(request):
     feature_films = Movie.objects.order_by('?')[:3]
-    context = {'feature_films': feature_films}
+    genre_list = Genre.objects.order_by('?')
+    context = {'feature_films': feature_films, 'genre_list': genre_list}
     return render(request, 'movie/index.html', context)
