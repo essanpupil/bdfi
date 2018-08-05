6677d973a4d31726357005a58f427e8afb3c326b
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2018-08-05 11:27:57 +0700

    Change template

--- movie/views.py
+++ movie/views.py
@@ -4,7 +4,7 @@ from movie.models import Movie, Genre
 
 
 def home(request):
-    feature_films = Movie.objects.order_by('?')[:3]
+    feature_films = Movie.objects.order_by('?')[:2]
     genre_list = Genre.objects.order_by('?')
     context = {'feature_films': feature_films, 'genre_list': genre_list}
     return render(request, 'movie/index.html', context)
