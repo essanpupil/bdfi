a646312f4ac6c1687ecbdc27cd98e5d854c50575
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2018-08-06 00:02:44 +0700

    register to admin site category, trivia & review

--- movie/admin.py
+++ movie/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from movie.models import Movie, Actor, Genre
+from movie.models import Movie, Actor, Genre, Category, Trivia, Review
 
 admin.site.register(Movie)
 admin.site.register(Actor)
 admin.site.register(Genre)
+admin.site.register(Category)
+admin.site.register(Trivia)
+admin.site.register(Review)
+
