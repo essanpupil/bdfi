8207f5384e8022cfc370b77c85d2e5ed26ad1007
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2018-08-06 00:03:05 +0700

    Dispaly newest movie as headline blog

--- movie/views.py
+++ movie/views.py
@@ -4,7 +4,12 @@ from movie.models import Movie, Genre
 
 
 def home(request):
-    feature_films = Movie.objects.order_by('?')[:2]
+    newest_film = Movie.objects.last()
+    feature_films = Movie.objects.exclude(id__in=[newest_film.id]).order_by('?')[:2]
     genre_list = Genre.objects.order_by('?')
-    context = {'feature_films': feature_films, 'genre_list': genre_list}
+    context = {
+        'feature_films': feature_films,
+        'genre_list': genre_list,
+        'newest_film': newest_film,
+    }
     return render(request, 'movie/index.html', context)
