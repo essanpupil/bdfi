91f3daa1b71770815aa420c6054590a47363c79f
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2018-08-10 00:43:28 +0700

    add models in admin

--- movie/admin.py
+++ movie/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from movie.models import Movie, Actor, Genre, Category, Trivia, Review
+from movie.models import Movie, Actor, Genre, Category, Trivia, Review, Role, ProductionHouse, \
+    Cast, Credit
 
 admin.site.register(Movie)
 admin.site.register(Actor)
@@ -8,4 +9,8 @@ admin.site.register(Genre)
 admin.site.register(Category)
 admin.site.register(Trivia)
 admin.site.register(Review)
+admin.site.register(Role)
+admin.site.register(ProductionHouse)
+admin.site.register(Cast)
+admin.site.register(Credit)
 
