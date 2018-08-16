6c8eb1e93a19570113be3c1af5c03a2aa97da585
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2018-08-17 00:29:44 +0700

    remove jumbotron in home page

--- movie/views.py
+++ movie/views.py
@@ -7,13 +7,11 @@ from movie.models import Movie, Genre, Actor
 
 
 def home(request):
-    newest_film = Movie.objects.last()
-    feature_films = Movie.objects.exclude(id__in=[newest_film.id]).order_by('?')[:2]
+    feature_films = Movie.objects.order_by('?')[:3]
     genre_list = Genre.objects.order_by('?')
     context = {
         'feature_films': feature_films,
         'genre_list': genre_list,
-        'newest_film': newest_film,
     }
     return render(request, 'movie/index.html', context)
 
